ff0b4cce1e52c3918623efe2d098171912abafe4
Julien LEROY <julien.leroy.work@gmail.com>
2026-07-31 15:13:26 +0200

    Delete old files

--- Backend/Mls.Patients.Api/Authentication/ApiUser.cs
+++
@@ -1,8 +0,0 @@
-namespace Mls.Patients.Api.Authentication
-{
-    public class ApiUser
-    {
-        public string Username { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
-    }
-}
