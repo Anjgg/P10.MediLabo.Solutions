160afd5ef9f5021418a5766cd9aff2ef0d5c4051
Julien LEROY <julien.leroy.work@gmail.com>
2026-07-31 13:26:47 +0200

    Update migration with error naming patient

--- Backend/Mls.Patients.Api/Migrations/20260731112153_Fix-Name-Patient.Designer.cs
+++ Backend/Mls.Patients.Api/Migrations/20260731112153_Fix-Name-Patient.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Mls.Patients.Api.Data;
 
@@ -11,9 +12,11 @@ using Mls.Patients.Api.Data;
 namespace Mls.Patients.Api.Migrations
 {
     [DbContext(typeof(PatientDbContext))]
-    partial class PatientDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260731112153_Fix-Name-Patient")]
+    partial class FixNamePatient
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Backend/Mls.Patients.Api/Migrations/20260731112153_Fix-Name-Patient.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mls.Patients.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixNamePatient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Nom",
+                value: "TestEarlyOnset");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Nom",
+                value: "TestEarlyOnse");
+        }
+    }
+}
