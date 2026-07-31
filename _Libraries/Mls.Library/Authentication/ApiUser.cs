872e4c55953afff598a91eb682b86be5ea735af0
Julien LEROY <julien.leroy.work@gmail.com>
2026-07-31 14:12:41 +0200

    Move common code into Library

---
+++ _Libraries/Mls.Library/Authentication/ApiUser.cs
@@ -0,0 +1,8 @@
+namespace Mls.Library.Authentication
+{
+    public class ApiUser
+    {
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
